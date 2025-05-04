c79a9eac37f289123f7f719a22c4cf61c759b433
khei4 <kk.asano.luxy@gmail.com>
2025-05-04 20:00:02 +0900

    update

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import { load } from 'https://deno.land/std@0.212.0/dotenv/mod.ts'
-import { createBot } from 'npm:@discordeno/bot@21.0.0'
+import { createBot } from 'discordeno'
+import * as log from "@std/log";
 
-
+log.setup({
+  handlers: {
+    default: new log.ConsoleHandler("INFO", {
+      formatter: log.formatters.jsonFormatter,
+      useColors: true,
+    }),
+  },
+});
 await load({export: true});
 
 
@@ -9,8 +17,8 @@ const bot = createBot({
   token: Deno.env.get("DISCORD_TOKEN")!,
   applicationId: Deno.env.get("APPLICATION_ID"),
   events: {
-    ready: ({ shardId }) => console.log(`Shard ${shardId} ready`),
-    async messageCreate(message) {await console.log(message);},
+    ready: ({ shardId }) => log.info(`Shard ${shardId} ready`),
+    async messageCreate(message) {await log.info(message);},
   },
 })
 
