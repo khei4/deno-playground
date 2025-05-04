9cc921d35b47222e6519a875cc6261be0e868cea
khei4 <kk.asano.luxy@gmail.com>
2025-05-04 19:49:36 +0900

    export .env

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { load } from 'https://deno.land/std@0.212.0/dotenv/mod.ts'
 import { createBot } from 'npm:@discordeno/bot@21.0.0'
 
-const env = await load();
+
+await load({export: true});
+
 
 const bot = createBot({
-  token: env.DISCORD_TOKEN,
-  applicationId: BigInt(env.APPLICATION_ID),
+  token: Deno.env.get("DISCORD_TOKEN")!,
+  applicationId: Deno.env.get("APPLICATION_ID"),
   events: {
     ready: ({ shardId }) => console.log(`Shard ${shardId} ready`),
     async messageCreate(message) {await console.log(message);},
